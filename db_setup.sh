b57827ef33c02358548c467eab1392e176dbafa5
Germán Ruelas <lgruelas@hotmail.com>
2018-04-29 21:04:38 -0500

    DB user

--- db_setup.sh
+++ db_setup.sh
@@ -2,8 +2,12 @@
 
 source dbconfig.sh
 
-echo "creating database..."
+echo "Creating database..."
 mysql -u $db_user -p$db_password -e "CREATE DATABASE IF NOT EXISTS desastres"
+
+echo "Creating user..."
+mysql -u $db_user -p$db_password -e "CREATE USER 'db_consultant'@'localhost' IDENTIFIED BY '$usr_password';"
+mysql -u $db_user -p$db_password -e "GRANT SELECT ON dedastres.* TO ‘db_consultant’@'localhost’;FLUSH PRIVILEGES;"
 
 echo "Restoring database data..." 
 mysql -u $db_user -p$db_password desastres < respaldo.sql
